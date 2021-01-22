eed8f8d571321eadb655395154de4fdd09b5d552
Mark Solinski <coach.marks6i@gmail.com>
2021-01-22 16:32:47 -0700

    First release

--- image_convert.py
+++ image_convert.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
+
 from PIL import Image, ImageOps
-from icecream import ic
 import sys
 import os
 from glob import glob
@@ -8,6 +9,55 @@ from glob import glob
 # global state variables
 background_color = (255, 255, 255)
 threshold = 0
+
+def print_help():
+    print('''
+image_convert - Python script to make it easy to do a few simple image
+                manipulations.  Mostly done so that I could convert
+                dark images to light images with white backgrounds
+                to save ink when I print them.  Best for dark mode apps
+                with text.
+
+syntax: python image_convert.py [ [ -<commands> ] <filenames> ]+
+
+        filenames - space-separated and/or wildcarded filenames
+        commands  - comma-separated list (no embedded spaces) of commands
+                    to apply to the following files. (details below)
+
+details:
+    commands:
+        i         - invert the image
+        tNNN      - threshold for low intensity.  'NNN' is a number from 0
+                    to 255. For example if 'NNN' were 32, then all pixels
+                    which had an intensity of less than or equal to 32
+                    would be set to 0 (black).
+        bHHHHHH   - background color.  'HHHHHH' is a triplet of hexadecimal
+                    characters representing the RGB values of the
+                    background.  For example: b000000 would be black and
+                    bFFFFFF would be white.  This is used if it is
+                    necessary to handle (semi-)transparency.
+        h,?       - prints this text
+
+examples:
+
+    * To invert an image
+    python image_convert.py -i image.png
+
+    * To force dark colors to black:
+    python image_convert.py -t32 image.png
+
+    * To force light colors to white:
+    python image_convert.py -i,t32,i image.png
+
+    * To invert one image and force dark colors to black in another
+    python image_convert.py -i image1.png -t32 image2.png
+
+    * To invert all images
+    python image_convert.py - i *.png *.jpg
+
+note:
+    Only tested for PNG and JPG files. Converted files lose their transparency.
+''')
 
 
 def set_command(cmds):
@@ -46,7 +96,7 @@ def set_command(cmds):
                 exit(4)
 
         elif command == 'h' or command == '?': # help screen
-            print('Help!')
+            print_help()
 
         else:
             print(f"Error: command '{command}' is not valid.", file = sys.stderr)
@@ -94,6 +144,7 @@ def set_background(color):
     background_color = color
     return background_color
 
+
 # ============ main program ================
 
 command_list = []
@@ -101,7 +152,8 @@ command_list = []
 for arg in sys.argv[1:]:
 
     if arg[0] == '-':
-        command_list = set_command(arg[1:].lower())
+        commands = arg[1:].lower()
+        command_list = set_command(commands)
         continue
 
     files = glob(arg)
@@ -110,9 +162,6 @@ for arg in sys.argv[1:]:
         im = Image.open(filename)
         original_format = im.format
         original_mode   = im.mode
-        ic(filename)
-        ic(im.format)
-        ic(im.mode)
 
         for cmd in command_list:
 
@@ -129,3 +178,4 @@ for arg in sys.argv[1:]:
 
         os.rename(filename, filename+'.bak')
         im.save(filename, original_format)
+        print(f"File '{filename}' has been converted with command '{commands}'")
